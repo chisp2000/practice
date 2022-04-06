7c0b0f119674c13b33fe06e9ec23d418ed7e26bb
chisp2000 <chisp2000@protonmail.com>
2022-04-06 12:54:28 -0400

    writetoHTML now works

--- scripts/writetoHTML.py
+++ scripts/writetoHTML.py
@@ -1,9 +1,29 @@
+
+
+writtenword = ""
+
+
 def choose_style():
-    take = str(input("\Enter the style you wish to use: "))
+    take = str(input("\nEnter the style you wish to use: "))
+    return take
 
-    if take == "h1":
-        writtenWord = str(input("\nEnter your string: "))
-    else:
-        return
 
-choose_style()
+def enter_info():
+    writtenWord = str(input("\nEnter your string: "))
+    return writtenWord
+
+def openFile(styles, infos):
+    file = open("website.html", "w")
+
+    styleBRACKETSopen = "<%s>" % (styles)
+    styleBRACKETSclosed = "</%s>" % (styles)
+    stringTOWEB = (styleBRACKETSopen, infos, styleBRACKETSclosed)
+
+    #file.write(stringTOWEB)
+    file.write("<%s>%s</%s>" % (styles, infos, styles))
+    file.close()
+
+style = choose_style()
+info = enter_info()
+
+openFile(style, info)
