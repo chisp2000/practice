f896995eb748fbbc40b50eac46fb38daf2a6c7a3
chisp2000 <chisp2000@protonmail.com>
2022-04-09 12:37:15 -0400

    function now works as intended, commented, needs to be tested further

--- scripts/det_Size.py
+++ scripts/det_Size.py
@@ -1,19 +1,21 @@
 import os
+from humanize import naturalsize
 
-directory = "/home/kali/Desktop/"
+directory = input("Please enter the directory you'd like to search: ")
+xsize = int(input("Please input which size you would like to search for: "))
 
 
 def sizeRETURN():
-
-    for (root,dirs, files) in os.walk(directory, followlinks=True):
-        print(os.path.abspath(""))
+    #looks for files in directory variable
+    for (root,dirs, files) in os.walk(directory, onerror=None):
+        #looks through each file in files
         for name in files:
-            print(name)
-            print(os.path.getsize(name))
-            #if os.path.getsize(name) > 1000000000:
-            #path = (os.path.join(root, name))
-            #size = os.path.getsize(path)
-
-
+            #path is the full path of the searched file
+            path = os.path.join(root, name)
+            #checks if file in path exists
+            isFile = os.path.isfile(path)
+            #checks if isFile is  true and also larger than xsize
+            if isFile and (os.stat(path).st_size >= xsize):
+                print(path, naturalsize(os.stat(path).st_size), "bytes")
 
 sizeRETURN()
